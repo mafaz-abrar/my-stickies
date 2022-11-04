e870263daba11235f3792a17aadae844278318fe
Mafaz Abrar Jan Chowdhury <mafazabrar@gmail.com>
2022-11-05 03:48:10 +1100

    Added New Note Activity, decided to stay with List View Adapter for the Adapter class for Recycler View.

---
+++ app/src/main/java/com/mafazabrar/mystickies/NewNoteActivity.kt
@@ -0,0 +1,11 @@
+package com.mafazabrar.mystickies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_note)
+    }
+}
